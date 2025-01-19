b6949d07629d74a449d3dab957c3e6df37236a78
Patrick Mendus <mendus59@gmail.com>
2025-01-18 23:54:42 -0800

    moved script to github workflows folder

--- .github/workflows/federal_register_api.py
+++ .github/workflows/federal_register_api.py
@@ -1,8 +1,21 @@
 
 import requests
-from pprint import pprint
-from ExecOrder import ExecOrder
+import json
 
+class ExecOrder:
+    def __init__(self, doc_number, pdf, pub_date, title):
+        self.document_number = doc_number
+        self.pdf_url = pdf
+        self.publication_date = pub_date
+        self.title = title
+    
+    def __str__(self):
+        return "Executive Order #" + self.document_number + ": " + self.title
+
+    def save(self):
+        filename = "order_objects/" + self.document_number + ".json"
+        with open(filename, "w+") as file:
+            json.dump(vars(self), file, indent=4)
 
 def process_exec_order(exec_object):
     exec_order = ExecOrder(
