094c580573748a89e74577e2f1650c7b165e3e7f
Patrick Mendus <mendus59@gmail.com>
2025-01-19 01:56:27 -0800

    Log file added, date changed to an argument

---
+++ ExecOrder.py
@@ -0,0 +1,24 @@
+import json
+
+class ExecOrder:
+    def __init__(self, doc_number, pdf, pub_date, title):
+        self.document_number = doc_number
+        self.pdf_url = pdf
+        self.publication_date = pub_date
+        self.title = title
+        self.summary = None
+    
+    def __str__(self):
+        return "Executive Order #" + self.document_number + ": " + self.title
+
+    def save(self):
+        filename = "order_objects/" + self.document_number + ".json"
+        try:
+            with open(filename, "x") as file:
+                json.dump(vars(self), file, indent=4)
+            print("Writing " + str(self))
+            with open("logs/write_log.txt", "a") as log:
+                log.write(str(self)+"\n")
+        except:
+            pass
+        return
--- federal_register_api.py
+++ federal_register_api.py
@@ -1,22 +1,7 @@
 
 import requests
-import json
-
-class ExecOrder:
-    def __init__(self, doc_number, pdf, pub_date, title):
-        self.document_number = doc_number
-        self.pdf_url = pdf
-        self.publication_date = pub_date
-        self.title = title
-    
-    def __str__(self):
-        return "Executive Order #" + self.document_number + ": " + self.title
-
-    def save(self):
-        filename = "order_objects/" + self.document_number + ".json"
-        with open(filename, "w+") as file:
-            json.dump(vars(self), file, indent=4)
-            print("File " + self.document_number + " written successfully")
+import sys
+from ExecOrder import ExecOrder
 
 def process_exec_order(exec_object):
     exec_order = ExecOrder(
@@ -28,11 +13,11 @@ def process_exec_order(exec_object):
     exec_order.save()
     return exec_order
 
-def main():
+def main(date):
     url = "https://federalregister.gov/api/v1/documents.json"
     params = {
         "conditions[presidential_document_type][]": "executive_order",
-        "conditions[publication_date][gte]": "2025-01-14"
+        "conditions[publication_date][gte]": date
     }
 
     results = requests.get(url=url, params=params)
@@ -41,9 +26,12 @@ def main():
         if results.json()["count"]:
             for result in results.json()["results"]:
                 exec_order = process_exec_order(result)
-                print(exec_order)
     except:
         print("error parsing fedregister result")
 
 if __name__=="__main__":
-    main()
+    try:
+        date = sys.argv[1]
+        main(date)
+    except:
+        print("Argument not provided")
