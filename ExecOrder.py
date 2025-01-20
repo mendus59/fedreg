efb3f5bf0fc7426426c1fb065b0fbea8a6f16f63
Patrick Mendus <mendus59@gmail.com>
2025-01-19 20:53:24 -0800

    Added call to AI summary API

--- ExecOrder.py
+++ ExecOrder.py
@@ -1,5 +1,7 @@
 import json
 import logging
+from pathlib import Path
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -14,14 +16,46 @@ class ExecOrder:
     def __str__(self):
         return "Executive Order #" + self.document_number + ": " + self.title
 
-    def save(self):
-        filename = "order_objects/" + self.document_number + ".json"
+    def get_url(self):
+        return "order_objects/" + self.document_number + ".json"
+
+    def save(self, api_key=None):
         try:
-            with open(filename, "x") as file:
-                json.dump(vars(self), file, indent=4)
-            logging.info("Writing " + str(self))
-            with open("logs/write_log.txt", "a") as log:
-                log.write(str(self)+"\n")
+            if not Path(self.get_url()).exists():
+                self.summarize(api_key)
+                
+                with open("logs/write_log.txt", "a") as log:
+                    log.write(str(self)+"\n")
+                with open(self.get_url(), "x") as file:
+                    json.dump(vars(self), file, indent=4)
+                logging.info("Object has been created: " + str(self))
+            else:
+                with open(self.get_url(), "r+") as file:
+                    data = file.read()
+                    if not json.loads(data)["summary"]:
+                        self.summarize(api_key)
+                        file.seek(0)
+                        file.truncate()
+                        json.dump(vars(self), file, indent=4)
+                        logging.info("Updated summary of " + str(self))
+
         except:
-            logging.info("Order exists: " + str(self))
+            logging.info("Error processing file: " + str(self))
         return
+
+    def summarize(self, api_key=None):
+        url = "https://api.apyhub.com/ai/summarize-documents/url"
+        headers = {
+            "apy-token": api_key,
+            "Content-Type": "application/json"
+        }
+        data = {
+            "url": self.pdf_url
+        }
+        try:
+            results = requests.post(url=url, headers=headers, json=data)
+            summary = results.json()["data"]["summary"]
+            self.summary = summary
+            logging.info("AI Summary call set for " + str(self))
+        except:
+            logging.info("AI Summary call failed")
--- federal_register_api.py
+++ federal_register_api.py
@@ -1,22 +1,38 @@
 
+import json
 import logging
+import os
 import requests
 import sys
 from ExecOrder import ExecOrder
 
 logger = logging.getLogger(__name__)
 
-def process_exec_order(exec_object):
+def process_exec_order(exec_object, api_key=None):
     exec_order = ExecOrder(
                     exec_object["document_number"],
                     exec_object["pdf_url"],
                     exec_object["publication_date"],
                     exec_object["title"]
                 )
-    exec_order.save()
+    exec_order.save(api_key)
     return exec_order
 
-def main(date):
+def audit(api_key=None):
+    for root, dirs, files in os.walk("./order_objects"):
+        for file in files:
+            file_path = os.path.join(root, file)
+            try:
+                with open(file_path, 'r') as f:
+                    json_object = json.loads(f.read())
+                    order = object.__new__(ExecOrder)
+                    order.__dict__ = json_object
+                    order.save(api_key)
+            except:
+                logging.info("Audit failed for: " + file_path)
+    
+
+def main(date, api_key=None):
     url = "https://federalregister.gov/api/v1/documents.json"
     params = {
         "conditions[presidential_document_type][]": "executive_order",
@@ -28,13 +44,15 @@ def main(date):
     try:
         if results.json()["count"]:
             for result in results.json()["results"]:
-                exec_order = process_exec_order(result)
+                exec_order = process_exec_order(result, api_key)
     except:
         logging.info("error parsing fedregister result")
+    
+    audit(api_key)
 
 if __name__=="__main__":
     logging.basicConfig(
-        filename='logs/full_log.log',
+        filename='logs/full_log.txt',
         format='%(asctime)s %(levelname)-8s %(message)s',
         level=logging.INFO,
         datefmt='%Y-%m-%d %H:%M:%S'
@@ -42,8 +60,9 @@ if __name__=="__main__":
     logging.info('Hourly fedreg script started')
     try:
         date = sys.argv[1]
+        api_key = sys.argv[2]
         logging.info("Date requested: " + date)
-        main(date)
+        main(date, api_key)
     except:
         logging.info("Argument not provided")
     logging.info('Fedreg API run completed')
