a2705a22d3c35337f09b6a2eeb9db685fe8098ee
Patrick Mendus <mendus59@gmail.com>
2025-01-24 09:52:19 -0800

    added latest update

--- load_data.js
+++ load_data.js
@@ -1,3 +1,14 @@
+function getLatestDate(data) {
+   // convert to timestamp and sort
+   var sorted_ms = data.map(function(item) {
+      return new Date(item.publication_date).toDateString()
+   }).sort(); 
+   // take latest
+   var latest_ms = sorted_ms[sorted_ms.length-1];
+   // convert to js date object 
+   return new Date(latest_ms).toUTCString();
+}
+
 async function load_data(){
     var data = {
         objects:[]
@@ -11,8 +22,9 @@ async function load_data(){
             .then((response) => response.json())
             .then((json) => data.objects.push(json))
     }
-
+    
     data.objects.sort((a, b) => b.publication_date.localeCompare(a.publication_date))
+    data.latest_date = getLatestDate(data.objects).toString()
     var exec_orders = Handlebars.compile(document.querySelector("#exec_orders").innerHTML);
     var loaded = exec_orders(data);
     document.querySelector("#content").innerHTML = loaded;
